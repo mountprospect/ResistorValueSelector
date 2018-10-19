800126e16511fd0f1c9bad8df6df2855902c9ea7
mountprospect <34148435+mountprospect@users.noreply.github.com>
2018-10-19 19:55:33 -0400

    Add files via upload
    Updated on 10/19/18 to remove println statements for testing. Cleaned up code.

--- ETwelveSeriesSelector.java
+++ ETwelveSeriesSelector.java
@@ -4,6 +4,7 @@ public class ETwelveSeriesSelector {
 
 /*
  * Created by Maalv on 10/18/18
+ * Updated by Maalv on 10/19/18
  */
 	
     static double lowestVal;
@@ -17,23 +18,17 @@ public class ETwelveSeriesSelector {
         if (initVal == 0 || initVal < 0) System.out.println("Not valid");
 		while (!(initVal > 0 && initVal < 10)) {
 			initVal /= 10;
-//			System.out.println(initVal + "After division");
 		}
 		if (initVal == 1) returnVal = resList[0];
-//		System.out.println(initVal + " AFTER FIRst");
 		for (int i = 1; i < resList.length; i++) {
 			baseVal = Math.abs(initVal - resList[0]);
 			iterativeVal = Math.abs(initVal - resList[i]);
-//			valThree = resList[i];
-//			System.out.println("baseVal, iterativeVal, ValThree " + baseVal + " "+ iterativeVal + " " + valThree);
 		    if (iterativeVal < baseVal && j == 0) {
 			lowestVal = iterativeVal;
 			j = 1;
-//			System.out.println(lowestVal + " lowestVal after j ==1");
 		    }
 		    if (iterativeVal < lowestVal && j != 0) {
 		        lowestVal = iterativeVal;
-//		        System.out.println(lowestVal + " after second if");
 		        returnVal = resList[i];
 		    }
 		}
